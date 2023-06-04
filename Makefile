df440e3e37413f43b4bceb6ace75383b58b55f8d
caelansar <819711623@qq.com>
2023-06-04 10:44:06 +0800

    build: add Makefile

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: setup
+setup:
+	cargo install wasm-pack
+	cd web && yarn
+
+.PHONY: start
+start: build_wasm
+	cd web && yarn start
+
+.PHONY: build_wasm
+build_wasm:
+	rm -rf pkg
+	wasm-pack build --target web
+
+.PHONY: build
+build: build_wasm
+	gsed "1c import { cae_print } from './index.js';" pkg/caescript_web.js > web/src/js/caescript_web.js
+	cp pkg/caescript_web_bg.wasm web/src/js/caescript_web_bg.wasm
+	cd web && yarn build
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,11 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const SRC_PATH = path.resolve(__dirname, 'src');
-const DIST_PATH = path.resolve(__dirname, 'dist');
+const DIST_PATH = path.resolve(__dirname, '../docs');
 
 module.exports = {
   mode: IS_PRODUCTION ? 'production' : 'development',
@@ -14,6 +13,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/js/index.js'),
 
   output: {
+    clean: true,
     path: DIST_PATH,
     filename: 'bundle.[hash].js',
   },
